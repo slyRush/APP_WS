4b515f4c7ce10eaa8018d7853b0ef9601640378f
Hery RAKOTONARIVO <hery@odity.fr>
2016-07-14 14:52:49 +0300

    changement adresse site, enelever port généré par phpstorm

--- Config/Config.js
+++ Config/Config.js
@@ -2,5 +2,5 @@
 //***************************************************************************//
 .constant('conf', {
     'entryPoint': 'http://10.2.150.239/fap_training/v1/',
-    'site': 'http://localhost:63342/APP_WS/'
+    'site': 'http://localhost/APP_WS/'
 });
